f58f04e433b0cba02f9e3613973dd2e22d784bb5
DESKTOP-JH6TVNS\Mikkel <mikkelkaasandersen@gmail.com>
2016-11-09 18:54:55 +0100

    v0.1.5

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://root:gummiand@ds035975.mlab.com:35975/jokedb');
-mongoose.connect('mongodb://root:gummiand@localhost/jokedb');
+mongoose.connect('mongodb://root:gummiand@ds035975.mlab.com:35975/jokedb');
+//mongoose.connect('mongodb://root:gummiand@localhost/jokedb');
 
 var site = require('./routes/site');
 var api = require('./routes/api');
